38be4b02510718c455bb02d87ce16ca422ed424a
ilias iosifidis <iosifidis90@gmail.com>
2025-11-08 17:41:03 +0200

    update

---
+++ Project-1/src/components/Block.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-80 bg-indigo-500 p-12 text-6xl shadow-md shadow-sky-300 text-yellow-400 rounded-xl" v-if="showBlock" @click="stopTimer">
+    click me
+  </div>
+</template>
+
+<script>
+export default{
+  props: ['delay'],
+  data(){
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock=true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods:{
+    startTimer(){
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer(){
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+}
+</script>
